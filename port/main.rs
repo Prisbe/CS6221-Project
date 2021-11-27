1b1172610da28da83f1e66549c2b44535b1844d6
Phoebe Chang <38013022+pchang07@users.noreply.github.com>
2021-11-27 15:28:40 -0800

    port scanner initial

--- port/main.rs
+++ port/main.rs
@@ -1,5 +1,4 @@
 use std::net::{SocketAddr, TcpStream, IpAddr, Ipv4Addr};
-use std::process;
 use std::env;
 use std::time::Duration;
 
@@ -13,42 +12,11 @@ fn main() {
     // mutable list to hold the open TCP ports
     let mut open_tcp_ports : Vec<u16> = Vec::with_capacity(MAX_PORT_NUMBER.into());
 
-    if args.len() > 1 {
-        // we specified a specific port, check that this is a valid port
-        let number = &args[1];
-        let specific_port: u16 = match number.parse() {
-            Ok(n) => {
-                n
-            },
-            Err(_) => {
-                eprintln!("Error: port provided is not a number");
-                process::exit(1);
-            },
-        };
-        //specific_port = &args[1].parse::<u16>();
-        if specific_port < MIN_PORT_NUMBER || specific_port > MAX_PORT_NUMBER {
-            println!("Port number provided is out of range.");
-            process::exit(1);
-        }
-        else {
-            scan_port(&mut open_tcp_ports, specific_port);
-            if open_tcp_ports.len() == 1 {
-                println!("Port {} is open.", specific_port);
-            }
-            else {
-                println!("Port {} is closed.", specific_port);
-            }
-        }
+    for port in MIN_PORT_NUMBER..=MAX_PORT_NUMBER {
+        scan_port(&mut open_tcp_ports, port);
     }
 
-    // if no specific port given:
-    else {
-        for port in MIN_PORT_NUMBER..=MAX_PORT_NUMBER {
-            scan_port(&mut open_tcp_ports, port);
-        }
-
-        print_open_ports(&mut open_tcp_ports);
-    }
+    print_open_ports(&mut open_tcp_ports);
 }
 
 fn scan_port(open_tcp_ports: &mut Vec<u16>, port: u16) {
